2ff3e34853162e1be8759d5aa4382c9a03315968
Eugen Yzeiri <eyzeiri@ritech.co>
2024-02-04 19:41:27 +0100

    Add username flag, askForConfirmation, promptForPassword

--- go.mod
+++ go.mod
@@ -10,6 +10,8 @@ require (
 	github.com/google/go-querystring v1.1.0 // indirect
 	golang.org/x/crypto v0.18.0 // indirect
 	golang.org/x/net v0.20.0 // indirect
+	golang.org/x/sys v0.16.0 // indirect
+	golang.org/x/term v0.16.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/protobuf v1.31.0 // indirect
 )
--- main.go
+++ main.go
@@ -5,18 +5,35 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"net/http"
 	"os/exec"
+	"strings"
+	"syscall"
 	"time"
 
 	"github.com/google/go-github/v39/github"
+	"golang.org/x/crypto/ssh/terminal"
 	"golang.org/x/oauth2"
 )
 
 var (
-	tokenFlag = flag.String("token", "", "GitHub Personal Access Token")
-	dateFlag  = flag.String("date", "", "When to push the commit, in the format: dd-mm-yyyy hh:mm")
-	pathFlag  = flag.String("path", "", "Path to the repository to commit")
+	tokenFlag    = flag.String("token", "", "GitHub Personal Access Token")
+	usernameFlag = flag.String("username", "", "GitHub username")
+	dateFlag     = flag.String("date", "", "When to push the commit, in the format: dd-mm-yyyy hh:mm")
+	pathFlag     = flag.String("path", "", "Path to the repository to commit")
 )
+
+func hasUpstreamBranch(repoPath string) (bool, error) {
+	cmd := exec.Command("git", "-C", repoPath, "rev-parse", "--abbrev-ref", "--symbolic-full-name", "@{u}")
+	err := cmd.Run()
+	if err != nil {
+		if _, ok := err.(*exec.ExitError); ok {
+			return false, nil // No upstream branch
+		}
+		return false, err // Some other error occurred
+	}
+	return true, nil // Upstream branch exists
+}
 
 func gitCommit(message string) {
 	cmd := exec.Command("git", "commit", "-m", message)
@@ -26,8 +43,14 @@ func gitCommit(message string) {
 	}
 }
 
-func gitPush(repoPath string) {
-	cmd := exec.Command("git", "-C", repoPath, "push", "origin", "HEAD")
+func gitPush(repoPath string, setUpstream bool) {
+	args := []string{"-C", repoPath, "push"}
+	if setUpstream {
+		args = append(args, "-u")
+	}
+	args = append(args, "origin", "HEAD")
+
+	cmd := exec.Command("git", args...)
 	err := cmd.Run()
 	if err != nil {
 		log.Fatal(err)
@@ -39,20 +62,57 @@ func parseDateTime(dateTimeStr string) (time.Time, error) {
 	return time.Parse(layout, dateTimeStr)
 }
 
+func askForConfirmation() bool {
+	var response string
+	fmt.Print("No upstream branch set. Would you like to set it automatically? [y/n]: ")
+	_, err := fmt.Scanln(&response)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return strings.ToLower(response) == "y" || strings.ToLower(response) == "yes"
+}
+
+func promptForPassword() string {
+	fmt.Print("Enter Password: ")
+	bytePassword, err := terminal.ReadPassword(int(syscall.Stdin))
+	if err != nil {
+		log.Fatalf("Error reading password: %v", err)
+	}
+	fmt.Println()
+	return string(bytePassword)
+}
+
 func main() {
 	flag.Parse()
 
-	if *tokenFlag == "" {
-		fmt.Println("GitHub Personal Access Token is required")
+	if *tokenFlag == "" && *usernameFlag == "" {
+		fmt.Println("A GitHub Username or a Personal Access Token is required")
 		return
 	}
 
-	ctx := context.Background()
-	ts := oauth2.StaticTokenSource(
-		&oauth2.Token{AccessToken: *tokenFlag},
-	)
-	tc := oauth2.NewClient(ctx, ts)
+	if *pathFlag == "" {
+		log.Fatal("Path to the repository is required")
+		return
+	}
+	var tc *http.Client
 
+	ctx := context.Background()
+
+	if *tokenFlag != "" {
+		ts := oauth2.StaticTokenSource(
+			&oauth2.Token{AccessToken: *tokenFlag},
+		)
+		tc = oauth2.NewClient(ctx, ts)
+	} else if *usernameFlag != "" {
+		password := promptForPassword()
+		tp := github.BasicAuthTransport{
+			Username: *usernameFlag,
+			Password: password,
+		}
+		tc = tp.Client()
+	} else {
+		log.Fatal("GitHub Personal Access Token or username is required")
+	}
 	client := github.NewClient(tc)
 	// The `client` variable is now authenticated and can be used to interact with GitHub.
 	// print the authenticated user
@@ -60,27 +120,34 @@ func main() {
 
 	if err != nil {
 		log.Fatal(err)
+		return
 	}
-
 	fmt.Println(*user.Login)
 
-	if *pathFlag == "" {
-		log.Fatal("Path to the repository is required")
-	}
-
+	// schedule the push
 	if *dateFlag != "" {
 		scheduledTime, err := parseDateTime(*dateFlag)
 		if err != nil {
 			log.Fatalf("Invalid date format: %v", err)
 		}
 
-		// wait until the scheduled time in a separate goroutine
+		upstreamExists, err := hasUpstreamBranch(*pathFlag)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		setUpstream := false
+		if !upstreamExists {
+			fmt.Println("The current branch does not have an upstream branch set.")
+			setUpstream = askForConfirmation()
+		}
+
+		// Schedule the push in a separate goroutine
 		go func() {
 			time.Sleep(time.Until(scheduledTime))
-			gitPush(*pathFlag)
+			gitPush(*pathFlag, setUpstream)
 		}()
 	} else {
-		// return an error if the date is not provided
 		log.Fatal("A date to schedule the commit is required")
 	}
 }
